6fa188f7dd04ad1cd90bf6aef0a500f4a9b00e57
rakdo <kublenko_p_v@sc.vsu.ru>
2019-11-16 18:18:00 +0300

    Добавьте файлы проекта.

---
+++ CarRentCompanyDotnet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarRentCompanyDotnet.Startup))]
+namespace CarRentCompanyDotnet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
